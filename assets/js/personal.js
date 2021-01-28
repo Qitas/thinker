5f48cb6141b0b84eda15f50c83a0c7e2acc47cef
caibingcheng <jack_cbc@163.com>
2021-01-28 16:48:55 +0800

    add live2d

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -18,7 +18,42 @@ function runtime() {
   let leavel3 = leavel2 % (60 * 1000);
   let seconds = Math.floor(leavel3 / (1000));
   let runbox = document.getElementById('run-time');
-  runbox.innerText = '本站已运行 ' + days + ' 天 ' + hours + ' 时 ' + minutes + ' 分 ' + seconds + ' 秒 ';
+  runbox.innerText = '本站已运行 ' + days + ' 天 ' + hours + ' 时 ' + minutes + ' 分 ' + ((seconds < 10) ? '0' : '') + seconds + ' 秒 ';
 }
 runtime();
 setInterval(runtime, 1000);
+
+document.addEventListener('visibilitychange', function () {
+  if (document.visibilityState == 'hidden') {
+    normal_title = document.title;
+    document.title = 'w(ﾟДﾟ)w崩溃啦！';
+  } else {
+    document.title = '(ε￣ *)骗你哒！';
+    setTimeout(function () {
+      document.title = normal_title;
+    }, 1000)
+  }
+});
+
+function loadExternalResource(url, type) {
+  return new Promise((resolve, reject) => {
+    let tag;
+
+    if (type === "css") {
+      tag = document.createElement("link");
+      tag.rel = "stylesheet";
+      tag.href = url;
+    }
+    else if (type === "js") {
+      tag = document.createElement("script");
+      tag.src = url;
+    }
+    if (tag) {
+      tag.onload = () => resolve(url);
+      tag.onerror = () => reject(url);
+      document.head.appendChild(tag);
+    }
+  });
+}
+
+loadExternalResource("/live2d/autoload.js", "js");
