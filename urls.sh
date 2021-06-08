2c71ae1008b45c08bc4fc9b5dc69edbb83ae7a29
caibingcheng <jack_cbc@163.com>
2021-06-08 13:39:31 +0800

    remove urls.sh

--- urls.sh
+++ urls.sh
@@ -1,11 +1,11 @@
-DIR=./content/posts/*
-echo > urls.txt
-for f in $DIR
-do
-    url_p=$(echo $f | awk -F '/' '{print $4}')
-    url="https://bbing.com.cn/$url_p/index.html"
-    echo $url >> urls.txt
-done
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=bbing.com.cn&token=UD2Ca59xBY9vmnOm"
-echo ''
-echo 'post urls success'
+# DIR=./content/posts/*
+# echo > urls.txt
+# for f in $DIR
+# do
+#     url_p=$(echo $f | awk -F '/' '{print $4}')
+#     url="https://bbing.com.cn/$url_p/index.html"
+#     echo $url >> urls.txt
+# done
+# curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=bbing.com.cn&token=UD2Ca59xBY9vmnOm"
+# echo ''
+# echo 'post urls success'
